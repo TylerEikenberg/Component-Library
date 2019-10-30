aae53917c7c7b43ef9f3e47e0a15bd42a1068f7f
Tyler Eikenberg <tylereikenberg8195@gmail.com>
2019-10-30 16:41:59 -0400

    primary and danger button done

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React from "react";
+import "./Button.css";
 
 // This is a functional component - just sent up a little differently as an arrow function!
-const Button = (props) => (
-    <button>
-        {props.label}
-    </button>
-)
+const Button = props => {
+  let classList = "";
+  let types = ["primary", "danger", "success", "warning"];
+  if (types.includes(props.type)) {
+    classList += ` button-${props.type}`;
+  }
+  if (props.large) {
+    classList += ` button-large`;
+  }
+  if (props.hover) {
+    classList += ` button:hover`;
+  }
+  if (props.lighter) {
+    classList += ` button.lighter`;
+  }
+
+  return <button className={classList}>{props.label}</button>;
+};
 
 export default Button;
--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -1,7 +1,12 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
+import React from "react";
+import { storiesOf } from "@storybook/react";
 
-import Button from './Button';
+import Button from "./Button";
 
-storiesOf('Button', module)
-    .add('Primary', () => <Button label="Primary" />)
+storiesOf("Button", module)
+  .add("Primary", () => <Button label="Primary" type="primary" hover />)
+  .add("Danger", () => <Button label="Danger" type="danger" />)
+  .add("Warning", () => <Button label="Warning" type="warning" />)
+  .add("Success", () => <Button label="Success" type="success" />)
+  .add("Large Primary", () => <Button label="Primary" type="primary" large />)
+  .add("Large Danger", () => <Button label="Danger" type="danger" large />);
