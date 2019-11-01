fd543af2e7b5967584d5639225fd7f4276ffbde9
Tyler Eikenberg <tylereikenberg8195@gmail.com>
2019-11-01 11:51:38 -0400

    working on thumbnail overlay

--- src/components/Button/Forms/Thumbnail.js
+++ src/components/Button/Forms/Thumbnail.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./Thumbnail.css";
 import laptopimg from "../assets/laptop.svg";
+import stars from "../assets/stars.svg";
 
 class Thumbnail extends Component {
   constructor(props) {
@@ -9,20 +10,29 @@ class Thumbnail extends Component {
     this.state = {
       mouseEnter: false
     };
+    this.didMouseEnter = this.didMouseEnter.bind(this);
   }
+
+  didMouseEnter = () => {
+    console.log("hey");
+    this.setState({ mouseEnter: true });
+  };
 
   render() {
     return (
       <div className="thumbnail-container">
-        <div className="image-container">
+        <div className="image-container" onMouseEnter={this.didMouseEnter}>
+          <div className="item-hover-overlay"></div>
           <img className="item-container" src={laptopimg}></img>
         </div>
         <div className="info-container">
           <div className="horizontal-line"></div>
-          <h4 className="info-title">300</h4>
-          <img className="info-rating"></img>
-          <h3 className="info-real-price"></h3>
-          <h3 className="info-old-price"></h3>
+          <h4 className="info-title">Apple MacBook Pro</h4>
+          <img className="info-rating" src={stars}></img>
+          <div className="price-container">
+            <h3 className="info-real-price">$300</h3>
+            <h3 className="info-old-price"> $300</h3>
+          </div>
         </div>
       </div>
     );
